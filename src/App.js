c92e2f0bc95f43bfe21234a7aec271f251558618
Gokulakrishna05 <gokulakrishna76@gmail.com>
2024-07-19 13:39:44 +0530

    account

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-
+import Secure from "./components/Secure";
+import Account from './pages/Account';
 
 function App() {
     return <div className="container">
@@ -11,7 +12,10 @@ function App() {
             <Routes>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/" element={<Chat/>}/>
+                <Route path="/" element={<Secure />}>
+                    <Route path="/" element={<Chat/>}/>
+                    <Route path="/account" element={<Account/>}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     </div>
