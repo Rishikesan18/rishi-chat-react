f65b3cb48807b6878023116abc9cd0db926ae81f
Rishikesan18 <p.rishikesan18@gmail.com>
2024-07-19 14:43:12 +0530

    send messages

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,77 +1,72 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {socket} from "../index";
 import axios from "axios";
-import { Context } from '../components/Secure';
+import {Context} from "../components/Secure";
+import {useParams} from "react-router-dom";
 
 const Chat = () => {
+    const {id} = useParams();
+    const [user] = useContext(Context);
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const [user] = useContext(Context);
-
 
     useEffect(() => {
-        socket.on('message', (msg) => {
-            setMessages(messages => [...messages, msg])
-        })
+        (async () => {
+            const {data} = await axios.get(`users/${id}/messages`)
+            setMessages(data.messages)
 
+            socket.on('message', (msg) => {
+                setMessages(messages => [...messages, msg])
+            })
+        })()
         return () => {
             socket.off('message')
         }
-    }, []);
+    }, [id]);
 
     const submit = async (e) => {
         e.preventDefault()
 
-        await axios.post('message', {
-            message
+        await axios.post('messages', {
+            receiver_id: id, 
+            content: message, 
+            type: 'text'
         })
 
         setMessage('')
     }
 
     return <>
+        <div id="head" className="py-3 lh-sm border-bottom">
+            <strong className="mb-1">Members: </strong>
+        </div>
 
+        <div id="conversation">
+            {messages.map(m => {
+                let html;
 
-
-                <div id="head" className="py-3 lh-sm border-bottom">
-                    <strong className="mb-1">Members: John Doe, Jane Smith</strong>
-                </div>
-
-                <div id="conversation">
-                    {messages.map(m => {
-                        return <div className="row pt-2" key={m}>
-                            <div className="col-6">
-                                <div className="alert d-inline-block alert-primary" role="alert">
-                                    {m}
-                                </div>
-                            </div>
-
-                            <div className="col-6">
-
-                            </div>
-                        </div>
-                    })}
-
-
-                    {/*<div className="row pt-2">*/}
-                    {/*    <div className="col-6">*/}
-
-                    {/*    </div>*/}
-
-                    {/*    <div className="col-6">*/}
-                    {/*        <div className="alert d-inline-block alert-success float-end" role="alert">*/}
-                    {/*            Hello there*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                </div>
-
-                <form id="reply" className="p-3 w-100" onSubmit={submit}>
-                    <div className="input-group">
-                        <input className="form-control" placeholder="Write a message" onChange={e => setMessage(e.target.value)}/>
+                if (m.type === "text") {
+                    const cls = m.sender.id === user.id ? "alert-primary float-end" : "alert-success";
+                    html = <div className={`alert d-inline-block ${cls}`} role="alert">
+                        {m.content}
                     </div>
-                </form>
+                } else {
+                    html = <img src={m.content} alt="image" className="w-100"/>
+                }
 
+                return <div className="row pt-2" key={m.id}>
+                    {m.sender.id === user.id ? <div className="col-6"/> : null}
+                    <div className="col-6">{html}</div>
+                    {m.receiver.id === user.id ? <div className="col-6"/> : null}
+                </div>
+            })}
+        </div>
+
+        <form id="reply" className="p-3 w-100" onSubmit={submit}>
+            <div className="input-group">
+                <input className="form-control" placeholder="Write a message" onChange={e => setMessage(e.target.value)}/>
+            </div>
+        </form>
     </>
 };
 
