79284dd9ecfe7c5c26e2dc75605d5dbecafcc7d8
Rishikesan18 <p.rishikesan18@gmail.com>
2024-07-19 14:17:20 +0530

    layout

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -32,22 +32,6 @@ const Chat = () => {
     return <>
 
 
-        <div className="row">
-            <div className="col-3 vh-100">
-                <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary">
-                    <div className="list-group list-group-flush border-bottom scrollarea">
-                        <a href="#" className="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
-                            <div className="d-flex w-100 align-items-center justify-content-between">
-                                <strong className="mb-1">John Doe</strong>
-                                <small>Wed</small>
-                            </div>
-                            <div className="col-10 mb-1 small">Last message</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-9 border">
 
                 <div id="head" className="py-3 lh-sm border-bottom">
                     <strong className="mb-1">Members: John Doe, Jane Smith</strong>
@@ -88,8 +72,6 @@ const Chat = () => {
                     </div>
                 </form>
 
-            </div>
-        </div>
     </>
 };
 
